089af8f913107ded5248633f5153a54c4e3d07c6
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-26 22:09:56 -0700

    update

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -2,5 +2,21 @@
 var app = angular.module('nbaRoutes');
 
 app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
-
+	$scope.teamData = teamData;
+	$scope.newGame = {};
+	$scope.showNewGameForm = false;
+	$scope.toggleNewGameForm = function() {
+		if($scope.showNewGameForm === true) {
+			$scope.showNewGameForm = false;
+		} else if($scope.showNewGameForm === false)
+			$scope.showNewGameForm = true;
+		};
+	});
+	if($routeParams.team === 'utahjazz') {
+		$scope.homeTeam = 'Utah Jazz' && $scope.logoPath = 'images/jazz-logo.png';
+	} else if($routeParams.team === 'losangeleslakers') {
+		$scope.homeTeam = 'Los Angeles Lakers' && $scope.logoPath = 'images/lakers-logo.png';
+	} else if($routeParams.team === 'miamiheat') {
+		$scope.homeTeam = 'Miami Heat' && $scope.logoPath = 'images/heat-logo.png';
+	};
 });
